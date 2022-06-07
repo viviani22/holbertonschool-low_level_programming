b1791161c83973338a9664bab8c0185cb71b9b34
viviani22 <vivianxhumetiku@gmail.com>
2022-06-07 06:02:35 -0700

    Added task 12

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int I;
+	int j;
+	int J;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (I = '0'; I <= '8'; I++)
+		{
+			for (j = '0'; j <= '9'; j++)
+			{
+				for (J = '0'; J <= '9'; J++)
+				{
+					putchar(i);
+					putchar(I);
+					putchar(' ');
+					putchar(j);
+					putchar(J);
+					if ((i != '9' || I != '8') || (j != '9' || J != '9'))
+                                        {
+                                                putchar(',');
+                                                putchar(' ');
+                                        }
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
