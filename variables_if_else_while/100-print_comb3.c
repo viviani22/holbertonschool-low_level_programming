e4ee0b000247021487def409967937ca0a3013b6
viviani22 <vivianxhumetiku@gmail.com>
2022-06-07 04:17:57 -0700

    Added task 10

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	char I;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (I = i+1; I <= '9'; I++)
+		{
+			putchar (i);
+			putchar (I);
+			if (i < '8')
+			{
+			putchar (',');
+			putchar (' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
