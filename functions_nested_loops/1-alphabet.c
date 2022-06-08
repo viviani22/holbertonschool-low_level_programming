7e29538a6a7e682795c49888d5b3d458870b73f5
viviani22 <vivianxhumetiku@gmail.com>
2022-06-08 07:14:17 -0700

    Added task 0

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,14 @@
 #include "main.h"
-
+/**
+ * print_alphabet - Entry point
+ *
+ *
+ * Return: void (Success)
+ */
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
