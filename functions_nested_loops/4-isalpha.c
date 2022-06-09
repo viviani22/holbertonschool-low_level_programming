48666b2c37ec72cb17b2bcd78094368144a0f503
viviani22 <vivianxhumetiku@gmail.com>
2022-06-09 00:23:26 -0700

    added task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ * @c: The character to print
+ *
+ * Return: If letter, return 1.
+ * Otherwise, 0 is returned.
+ */
+
+int _isalpha(int c)
+{
+	if (c > '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
