b9d54cdec626a92d5561fe5252f22c4ae7226c8b
viviani22 <vivianxhumetiku@gmail.com>
2022-06-09 02:07:54 -0700

    Added task 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * add  - Entry point
- * 
- * Return - Sum of two integers 
+ *
+ * Return - Sum of two integers
  */
 
 int add(int a, int b)
 {
 	int sum;
-	
+
 	sum = a + b;
 	return (sum);
 }
