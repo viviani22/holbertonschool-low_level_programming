01f010a4e37d0a3b3db40eed5b4d6fbd7487c192
viviani22 <vivianxhumetiku@gmail.com>
2022-06-09 01:08:38 -0700

    Added task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: void (Succesful) | Prints every min of the day
+ */
+void jack_bauer(void)
+{
+	int hours, minutes;
+	
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
