347dd558296dd9ae013b349d2db8014ae2c99033
viviani22 <vivianxhumetiku@gmail.com>
2022-06-09 07:23:46 -0700

    Added task 13

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,29 +1,20 @@
 #include<stdio.h>
 /**
- * _natural - Entry point
- *
- * Return: void
- */
-void _natural(void)
-{
-	int a, sum;
-
-	for (a = 1; a < 1024; a++)
-	{
-		if ((a % 3 == 0) || (a % 5 == 0))
-		{
-			sum = sum + a;
-		}
-	}
-	printf("%d\n", sum);
-}
-/**
  * main - calls _natural
  *
  * Return: Always 0;
  */
 int main(void)
 {
-	_natural();
+	int a, sum;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			sum = sum + a;
+		}
+	}
+	printf("%i\n", sum);
 	return (0);
 }
