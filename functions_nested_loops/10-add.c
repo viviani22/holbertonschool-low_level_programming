df028260662e41e5d7b2f45b3bf4e0bf26b85bae
viviani22 <vivianxhumetiku@gmail.com>
2022-06-09 02:06:25 -0700

    added task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add  - Entry point
+ * 
+ * Return - Sum of two integers 
+ */
+
+int add(int a, int b)
+{
+	int sum;
+	
+	sum = a + b;
+	return (sum);
+}
--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
 			}
-			if (row != 9)
+			if (row < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
