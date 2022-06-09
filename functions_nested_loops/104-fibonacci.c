14dadc07f642553ef405de2c6277acdbab85d977
viviani22 <vivianxhumetiku@gmail.com>
2022-06-09 10:30:30 -0700

    Added tasl 16

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succesful)
+ */
+
+int main(void)
+{
+	unsigned long first_number, second_number, fib_number;
+	int i;
+
+	first_number = 1;
+	second_number = 2;
+	printf("%ld, %ld", first_number, second_number);
+	for (i = 3; i < 101; ++i)
+	{
+		fib_number = first_number + second_number;
+		printf(", %ld", fib_number);
+		first_number = second_number;
+		second_number = fib_number;
+	}
+	putchar('\n');
+	return (0);
+}
