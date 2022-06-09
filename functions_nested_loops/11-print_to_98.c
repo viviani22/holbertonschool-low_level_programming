472bb352864d234a32dfe98a139a8fec6fadd47e
viviani22 <vivianxhumetiku@gmail.com>
2022-06-09 03:41:58 -0700

    Added task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "main.h"
 /**
  * print_to_98 - Entry point
@@ -12,32 +12,35 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
+	for (i = n; i <= 98; i++)
+	{
+		if (i != 98)
 		{
-			printf("%d", 'i');
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%i, ", i);
 		}
-		_putchar('\n');
+		else
+		{
+			printf("%i\n", i);
+		}
+	}
 	}
 	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d", i);
 			if (i != 98)
 			{
-				_putchar(',');
-				_putchar(' ');
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d\n", i);
 			}
 		}
-		_putchar('\n');
 	}
 	else
 	{
-		printf("%d", n);
+		printf("%d\n", n);
 	}
 }
+
