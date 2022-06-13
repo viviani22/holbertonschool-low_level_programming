9fe55106aa0244975bbdead8a6d7ed367c60bbc3
viviani22 <vivianxhumetiku@gmail.com>
2022-06-13 02:04:25 -0700

    Added task 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_numbers - Prints when function is called
- * 
+ *
  * Return: void
  */
 
@@ -9,9 +9,9 @@ void print_numbers(void)
 {
 	int i;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i);
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
