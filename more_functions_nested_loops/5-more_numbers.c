ac653dcb56545d48051a17d9acfa0f819da61b9c
viviani22 <vivianxhumetiku@gmail.com>
2022-06-13 02:35:50 -0700

    Added task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - Called from main
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int row;
+
+	for (row = 0; row < 10; row++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
