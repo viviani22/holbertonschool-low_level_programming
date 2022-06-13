bfa93ace7cc7bb4157d30ba0dcda4a5fa1fc1122
viviani22 <vivianxhumetiku@gmail.com>
2022-06-13 01:44:40 -0700

    Added task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Takes two int from main
+ * @a: first int
+ * @b: second int
+ * Return: Multiplication of two int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
