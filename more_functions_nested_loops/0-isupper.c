c559d726b1367a51a86a850132f9ac0f09d1117e
viviani22 <vivianxhumetiku@gmail.com>
2022-06-13 01:17:31 -0700

    Added task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * isupper - Takes value c
+ * @c: Value taken by main
+ * Return: 1 for upper, 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c > 'Z')
+{
+	return (0);
+}
+else
+{
+	return (1);
+}
+}
