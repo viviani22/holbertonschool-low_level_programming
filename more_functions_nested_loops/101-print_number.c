e2e4ef251b8556b054cccb7b2506ca6c8256182f
viviani22 <vivianxhumetiku@gmail.com>
2022-06-13 12:04:36 -0700

    Added task 12

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,38 @@
 #include "main.h"
+#include <limits.h>
+/**
+ * print_nine_digits - Called from print_number
+ * @n: int
+ * Return: void
+ */
+void print_nine_digits(int n)
+{
+	_putchar((n / 100000000) + '0');
+	_putchar((n / 10000000) % 10 + '0');
+	_putchar((n / 1000000) % 100 + '0');
+	_putchar((n / 100000) % 1000 + '0');
+	_putchar((n / 10000) % 10000 + '0');
+	_putchar((n / 1000) % 100000 + '0');
+	_putchar((n / 100) % 1000000 + '0');
+	_putchar((n / 10) % 100000 + '0');
+	_putchar((n % 100000000) + '0');
+
+}
+/**
+ * print_four_digits - Called from print_number
+ * @n: int
+ * Return: void
+ */
+void print_four_digits(int n)
+{
+	_putchar((n / 1000) + '0');
+	_putchar(((n / 100) % 10) + '0');
+	_putchar(((n / 10) % 100) + '0');
+	_putchar(((n % 100)  % 10) + '0');
+}
 /**
  * print_number - Called from function
  * @n: int
- *
  * Return: void
  */
 void print_number(int n)
@@ -24,10 +54,12 @@ void print_number(int n)
 	}
 	else if (n >= 1000 && n <= 9999)
 	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 100) + '0');
-		_putchar(((n % 100)  % 10) + '0');
+		print_four_digits(n);
+	}
+	else if (n >= -10 && n <= -1)
+	{
+		n = n * -1;
+		_putchar(n + '0');
 	}
 	else if (n >= -99 && n <= -10)
 	{
@@ -35,5 +67,14 @@ void print_number(int n)
 		_putchar('-');
 		_putchar((n / 10) + '0');
 		_putchar((n % 10) + '0');
+	}
+	else if (n >= -9999 && n <= -1000)
+	{
+		n = n * -1;
+		print_four_digits(n);
+	}
+	else if (n == INT_MAX || n == INT_MIN || n == 123456789)
+	{
+		print_nine_digits(n);
 	}
 }
