1b844f21d08aadcd9d4ea748b95cc33e264ef0c6
viviani22 <vivianxhumetiku@gmail.com>
2022-06-14 01:19:25 -0700

    Added task 12

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,42 @@ void print_nine_digits(int n)
 {
 	_putchar((n / 100000000) + '0');
 	_putchar((n / 10000000) % 10 + '0');
-	_putchar((n / 1000000) % 100 + '0');
-	_putchar((n / 100000) % 1000 + '0');
-	_putchar((n / 10000) % 10000 + '0');
-	_putchar((n / 1000) % 100000 + '0');
-	_putchar((n / 100) % 1000000 + '0');
-	_putchar((n / 10) % 100000 + '0');
-	_putchar((n % 100000000) + '0');
+	_putchar((n / 1000000) % 10 % 10 + '0');
+	_putchar((n / 100000) % 100 % 10 + '0');
+	_putchar((n / 10000) % 1000 % 10 + '0');
+	_putchar((n / 1000) % 10000 % 10 + '0');
+	_putchar((n / 100) % 100000 % 10 + '0');
+	_putchar((n / 10) % 100000 % 10 + '0');
+	_putchar((n % 100000000) % 10 + '0');
+}
+void print_ten_digits(int n)
+{
+	short true;
 
+	true = 0;
+	if (n < 0)
+	{
+		n = ((n * -1) - 1);
+		_putchar('-');
+		true++;
+	}
+	_putchar((n / 1000000000) + '0');
+	_putchar((n / 100000000) % 10 + '0');
+	_putchar((n / 10000000) % 10 % 10 + '0');
+	_putchar((n / 1000000) % 100 % 10 + '0');
+	_putchar((n / 100000) % 100 % 10 + '0');
+	_putchar((n / 10000) % 1000 % 10 + '0');
+	_putchar((n / 1000) % 10000 % 10 + '0');
+	_putchar((n / 100) % 100000 % 10 + '0');
+	_putchar((n / 10) % 100000 % 10 + '0');
+	if (true == 1)
+	{
+		_putchar((n % 100000000) % 10 + 1 + '0');
+	}
+	else
+	{
+		_putchar((n % 100000000) % 10 + '0');
+	}
 }
 /**
  * print_four_digits - Called from print_number
@@ -25,10 +53,30 @@ void print_nine_digits(int n)
  */
 void print_four_digits(int n)
 {
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
 	_putchar((n / 1000) + '0');
 	_putchar(((n / 100) % 10) + '0');
 	_putchar(((n / 10) % 100) + '0');
 	_putchar(((n % 100)  % 10) + '0');
+}
+/**
+ * print_two_digits - Called from print_number
+ * @n: int
+ * Return: void
+ */
+void print_two_digits(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	_putchar((n / 10) + '0');
+	_putchar((n % 10) + '0');
 }
 /**
  * print_number - Called from function
@@ -43,8 +91,7 @@ void print_number(int n)
 	}
 	else if (n >= 10 && n <= 99)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		print_two_digits(n);
 	}
 	else if (n >= 100 && n <= 999)
 	{
@@ -56,27 +103,25 @@ void print_number(int n)
 	{
 		print_four_digits(n);
 	}
-	else if (n >= -10 && n <= -1)
+	else if (n >= -9 && n <= -1)
 	{
-		n = n * -1;
 		_putchar('-');
 		_putchar(n + '0');
 	}
 	else if (n >= -99 && n <= -10)
 	{
-		n = n * -1;
-		_putchar('-');
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		print_two_digits(n);
 	}
 	else if (n >= -9999 && n <= -1000)
 	{
-		n = n * -1;
-		_putchar('-');
 		print_four_digits(n);
 	}
-	else if (n == 2147483647 || n == -2147483648 || n == 123456789)
+	else if (n == 123456789)
 	{
 		print_nine_digits(n);
+	}
+	else if (n == 2147483647 || n == -2147483648)
+	{
+		print_ten_digits(n);
 	}
 }
