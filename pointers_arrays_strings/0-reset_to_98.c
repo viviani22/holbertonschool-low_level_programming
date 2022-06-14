2827b24501310106a4b18421a74a02bc4881cb6f
viviani22 <vivianxhumetiku@gmail.com>
2022-06-14 04:54:00 -0700

    Added task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * reset_to_98 -
- * @*n: pointer to int
+ * reset_to_98 - Called from main
+ * @n: pointer to int
  * Return: void
  */
 void reset_to_98(int *n)
 {
 	int *p;
-	
+
 	p = n;
 	*p = 98;
 }
