9b726d5889e6c28884886f5fcb4e832717d68097
viviani22 <vivianxhumetiku@gmail.com>
2022-06-15 11:44:32 -0700

    Added task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,11 +6,12 @@
  */
 int _atoi(char *s)
 {
-	int i, negative;
+	int i, negative, is_int;
 	unsigned int result;
 
 	result = 0;
 	negative = 1;
+	is_int = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[0] == '-')
@@ -20,11 +21,11 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			result = result * 10 + (s[i] - '0');
-		}
-		else
-		{
-			return (0);
+			is_int = 1;
 		}
 	}
-	return (result * negative);
+	if (is_int == 1)
+		return (result * negative);
+	else
+		return (0);
 }
