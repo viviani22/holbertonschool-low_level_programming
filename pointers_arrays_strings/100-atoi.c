fa6374777a57b36635c50834f58718cdaa9e0d3b
viviani22 <vivianxhumetiku@gmail.com>
2022-06-15 11:34:13 -0700

    Added task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _atoi - Called from main
+ * @s: pointer to char
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int i, negative;
+	unsigned long result;
+
+	result = 0;
+	negative = 1;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[0] == '-')
+		{
+			negative = -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (result * negative);
+}
