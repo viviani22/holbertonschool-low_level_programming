9cca7839440e983aebc0ddfcd13f74c2091a7a21
viviani22 <vivianxhumetiku@gmail.com>
2022-06-15 01:07:40 -0700

    Added task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - Called from function
+ * @s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (n = i; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
