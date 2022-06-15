7b57518ca87a3215ccb2d74ed49464149b1e3dd7
viviani22 <vivianxhumetiku@gmail.com>
2022-06-15 09:48:37 -0700

    Added task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,14 +6,23 @@
  */
 void puts_half(char *str)
 {
-	int i, half;
+	int i, half, j;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
-	for (half = (i - 1) / 2; str[half] != '\0'; half++)
+	if (i % 2 == 1)
 	{
-		_putchar(str[half]);
+		half = (i + 1) / 2;
 	}
+	else
+	{
+		half = i / 2;
+	}
+	for (j = half; j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+
 	_putchar('\n');
 }
