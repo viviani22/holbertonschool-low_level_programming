7fd2da6970192431101b36e801529e1c4286b7cb
viviani22 <vivianxhumetiku@gmail.com>
2022-06-16 09:23:16 -0700

    Added task 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcmp = Called from main
+ * @s1: pointer to char
+ * @s2: pointer to char
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, res;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s2[i] != '\0' && s2[i] != s1[i])
+		{
+			res = s1[i] - s2[i];
+			break;
+		}
+		else if (s1[i] == s2[i])
+		{
+			res = 0;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (res);
+}
