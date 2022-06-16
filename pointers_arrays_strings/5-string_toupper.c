461d256df77da712f2d21b2a37c871233462e768
viviani22 <vivianxhumetiku@gmail.com>
2022-06-16 09:38:09 -0700

    Added task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - called from main
+ * @s: pointer
+ * Return: pointer to char
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
