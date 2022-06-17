6ae566a3022d6f19ea2d6d2b6d70f71097aa4489
viviani22 <vivianxhumetiku@gmail.com>
2022-06-17 10:24:43 -0700

    Added task 100

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - called from main
+ * @s: pointer to pointer to char
+ * @to: pointer to char
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
