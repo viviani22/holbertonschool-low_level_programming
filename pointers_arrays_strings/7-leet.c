4c808b44ab2c70659b352c84d9c1a8c953c12f7f
viviani22 <vivianxhumetiku@gmail.com>
2022-06-17 01:51:37 -0700

    Added tasl 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,12 +7,12 @@
 char *leet(char *str)
 {
 	int i, swp;
-	char letters[11] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
-	char leet_num[11] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+	char letters[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char leet_num[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (swp = 0; swp <= 4; swp++)
+		for (swp = 0; swp < 10; swp++)
 		{
 			if (str[i] == letters[swp])
 			{
