9eed20748314ee1a9efbf4c01ecaa787e3e58a54
viviani22 <vivianxhumetiku@gmail.com>
2022-06-22 04:21:25 -0700

    Added task 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -8,18 +8,21 @@
  */
 int main(int argc, char **argv)
 {
-	int i;
+	int i, j;
 	int sum = 0;
 
 	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			sum = sum + atoi(argv[i]);
-			if (*argv[i] < '0' || *argv[i] > '9')
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
+	}
 	printf("%d\n", sum);
 
 	return (0);
