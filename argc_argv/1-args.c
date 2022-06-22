903b44dd531315d9434f2099429746d70262db42
viviani22 <vivianxhumetiku@gmail.com>
2022-06-22 00:55:53 -0700

    Added task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - start
+ * @argc: arg count
+ * @argv: arg array
+ * Return: int
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%i\n", argc);
+	return (0);
+}
