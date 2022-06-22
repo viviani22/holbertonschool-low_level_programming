0fa05c4a1b9186eb2c766ba6707ea6d664a8dd69
viviani22 <vivianxhumetiku@gmail.com>
2022-06-22 05:20:33 -0700

    Added task 5

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -16,34 +16,29 @@ int main(int argc, char **argv)
 		return (1);
 	}
 	money = atoi(argv[1]);
-	while (money != 0)
+	while (money > 0)
 	{
-		cents += 1;
 		if (money >= 25)
 		{
 			money = money - 25;
-			continue;
 		}
 		else if (money >= 10)
 		{
 			money = money - 10;
-			continue;
 		}
 		else if (money >= 5)
 		{
 			money = money - 5;
-			continue;
 		}
 		else if (money >= 2)
 		{
 			money = money - 2;
-			continue;
 		}
 		else if (money >= 1)
 		{
 			money = money - 1;
-			continue;
 		}
+		cents += 1;
 	}
 	printf("%d\n", cents);
 	return (0);
