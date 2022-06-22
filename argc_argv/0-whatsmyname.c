356ce98b988076879a8160fe00b544d308d69bdf
viviani22 <vivianxhumetiku@gmail.com>
2022-06-22 00:50:35 -0700

    Added task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - start
+ * @argc: arg count
+ * @argv: arg array
+ * Return: int
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
