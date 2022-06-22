b433c05ea2566c2ec08042691b10e7c172641014
viviani22 <vivianxhumetiku@gmail.com>
2022-06-22 01:42:12 -0700

    Added task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,15 +14,13 @@ int main(int argc, char **argv)
 
 	if (argc !=  3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		mul = a * b;
-		printf("%d\n", mul);
-	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a * b;
+	printf("%d\n", mul);
+
 	return (0);
 }
