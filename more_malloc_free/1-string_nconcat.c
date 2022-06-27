22eaa67a502befd5150b2f27a85d910b5c13c21e
viviani22 <vivianxhumetiku@gmail.com>
2022-06-27 02:21:58 -0700

    Added task 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * string_nconcat - called from main
+ * @s1: char
+ * @s2: char
+ * @n: unsigned int
+ * Return: char
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, size1 = sizeof(s1), size2 = sizeof(s2);
+	char *p;
+
+	if (n >= size2)
+	{
+		p = malloc(size1 + size2);
+	}
+	else
+	{
+		p = malloc(size1 + n);
+	}
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size1 && s1[i] != '\0'; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (j = 0; j < n && j < size2 && s2[j] != '\0'; j++)
+	{
+		p[i] = s2[j];
+		i++;
+	}
+	return (p);
+}
