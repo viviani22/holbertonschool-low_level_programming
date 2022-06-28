b7d98fa4001a76f4ab9cc9517422e58c8f44cbdf
viviani22 <vivianxhumetiku@gmail.com>
2022-06-28 03:09:45 -0700

    Fix task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
 /**
  * init_dog - called from main
@@ -11,7 +9,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		printf("Ok");
+		free(d);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
