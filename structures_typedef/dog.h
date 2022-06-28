511ab9c1171915576df866795e583864a09045fa
viviani22 <vivianxhumetiku@gmail.com>
2022-06-28 02:31:45 -0700

    Updated header

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -12,5 +12,11 @@ struct dog
 	float age;
 	char *owner;
 };
+#include <stdlib.h>
+#include <stdio.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
