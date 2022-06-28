c777feca0ef61990862e718f9079f95ff6fee9d5
viviani22 <vivianxhumetiku@gmail.com>
2022-06-27 19:20:18 -0700

    Fix task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -33,11 +33,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < size1 && s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		p[i] = s1[i];
 	}
-	for (j = 0; j < n && j < size2 && s2[j] != '\0'; j++)
+	for (j = 0; j < n && s2[j] != '\0'; j++)
 	{
 		p[i] = s2[j];
 		i++;
