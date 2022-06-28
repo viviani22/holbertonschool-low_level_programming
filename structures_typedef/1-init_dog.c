30c03a425edc9c284f5bf8ee37879c7b2dd0776c
viviani22 <vivianxhumetiku@gmail.com>
2022-06-28 02:20:55 -0700

    Added task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
  * init_dog - called from main
  * @d: dog id
@@ -10,8 +9,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	printf("%s\n %f\n %s\n", name, age, owner);
-	d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
