f34229642ff0d714102750226183fbd20cf54ec9
viviani22 <vivianxhumetiku@gmail.com>
2022-06-28 01:38:14 -0700

    added task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog info
+ * @name: string
+ * @age: float
+ * @owner: string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
