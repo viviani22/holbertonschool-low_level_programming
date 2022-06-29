a7765c03847b29ddca2d71be7d1e25277b99405a
viviani22 <vivianxhumetiku@gmail.com>
2022-06-29 00:11:40 -0700

    fix task 3, 4

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef ABS
-#define ABS(x) ((x) < 0 ? ((x) * (-1)) : (x))
+#ifndef _FUNCTION_LIKE_MACRO
+#define _FUNCTION_LIKE_MACRO
+#define ABS ((x) < 0 ? ((x) * (-1)) : (x))
 #endif
--- preprocessor/4-sum.h
+++ preprocessor/4-sum.h
@@ -1,3 +1,4 @@
-#ifndef SUM
+#ifndef _SUM
+#define _SUM
 #define SUM(x, y) ((x) + (y))
 #endif
