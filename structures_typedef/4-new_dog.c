abb5f9ab34fdb24f6e7c3678f3b737f91a6d053c
viviani22 <vivianxhumetiku@gmail.com>
2022-06-29 02:47:08 -0700

    fix task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,6 +9,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
+	char *cpname, *cpowner;
 
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
@@ -16,8 +17,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(my_dog);
 		return (NULL);
 	}
-	my_dog->name = name;
+	my_dog->name = malloc(sizeof(name));
+	if (my_dog->name == NULL)
+	{
+		free(my_dog);
+		return (NULL);
+	}
+	my_dog->owner = malloc(sizeof(owner));
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog->name);
+		free(my_dog);
+		return (NULL);
+	}
+	cpname = name;
+	cpowner = owner;
+	my_dog->name = cpname;
 	my_dog->age = age;
-	my_dog->owner = owner;
+	my_dog->owner = cpowner;
 	return (my_dog);
 }
