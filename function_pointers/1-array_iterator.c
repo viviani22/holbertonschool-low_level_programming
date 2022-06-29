4dcd2587d020d049f435c46f22ee56082e527b09
viviani22 <vivianxhumetiku@gmail.com>
2022-06-29 00:55:09 -0700

    Add task 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - called from main
+ * @array: pointer to int
+ * @size: size bit
+ * @action: pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL)
+		exit(15);
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
