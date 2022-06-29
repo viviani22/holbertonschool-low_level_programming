90f9e9e1784c1fb53de890f9117c238e19bb10c8
viviani22 <vivianxhumetiku@gmail.com>
2022-06-29 00:48:04 -0700

    Add task 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,9 +2,11 @@
 /**
  * print_name - called from main
  * @name: string
- * f - pointer to function
+ * @f: pointer to function
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		exit(15);
 	f(name);
 }
