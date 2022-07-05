840139090faa9d5665bba1cf8769db5948552d12
viviani22 <vivianxhumetiku@gmail.com>
2022-07-05 00:21:14 -0700

    add prototypes

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -7,4 +7,13 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_c(va_list p);
+void print_i(va_list p);
+void print_f(va_list p);
+void print_s(va_list p);
+struct format
+{
+	char type;
+	void (*f)(va_list);
+};
 #endif
