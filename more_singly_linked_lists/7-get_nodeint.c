dee099e1ef49fb6806e50ae327795e9685c61df2
viviani22 <vivianxhumetiku@gmail.com>
2022-07-13 22:18:23 -0700

    task 7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -14,11 +14,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (index == 0)
 		return (ptr);
-	for (i = 1; i <= index; i++)
+	for (i = 1; i <= index && ptr != NULL; i++)
 	{
 		ptr = ptr->next;
 	}
-	if (ptr == NULL)
+	if ((i - 1) != index)
 		return (NULL);
 	return (ptr);
 }
