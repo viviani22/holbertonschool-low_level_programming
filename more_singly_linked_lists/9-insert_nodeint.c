23ec22f8ad36c10a2f35880936e7792564f6719f
viviani22 <vivianxhumetiku@gmail.com>
2022-07-14 01:36:17 -0700

    task 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new->next = ptr;
-		ptr = new;
+		*head = new;
 		return (new);
 	}
 	for (i = 1; i <= idx - 1; i++)
