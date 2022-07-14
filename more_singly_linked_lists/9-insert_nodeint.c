8b2a56374027d4628a6fd7128d4acaa949145c64
viviani22 <vivianxhumetiku@gmail.com>
2022-07-13 22:43:39 -0700

    task 9

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * insert_nodeint_at_index - called from main
+ * @head: head
+ * @idx: unsigned int
+ * @n: int
+ * Return: node address
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *ptr = *head;
+	listint_t *new;
+	unsigned int i;
+
+	if (idx == 0)
+	{
+		new = malloc(sizeof(listint_t));
+		new->n = n;
+		new->next = ptr;
+		ptr = new;
+		return (ptr);
+	}
+	for (i = 1; i <= idx - 1 && ptr != NULL; i++)
+	{
+		ptr = ptr->next;
+	}
+	if ((i - 1) != idx)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	new->n = n;
+	new->next = ptr->next;
+	ptr = new;
+	return (ptr);
+}
