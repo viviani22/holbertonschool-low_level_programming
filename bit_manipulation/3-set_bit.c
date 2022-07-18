c69b7a1fd26fbae5f0f87755ce131dffbf300c27
viviani22 <vivianxhumetiku@gmail.com>
2022-07-18 07:00:47 -0700

    task 3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * set_bit - called from main
+ * @n: int
+ * @index: position
+ * Return: -1 if failed
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (*n >> index & 1)
+		return (1);
+	else
+	{
+		*n = *n | (1 << index);
+		return (1);
+	}
+	return (-1);
+}
