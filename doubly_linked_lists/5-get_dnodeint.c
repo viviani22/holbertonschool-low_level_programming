d53477ff1003aa300a588e9768eb1c51d7df978c
viviani22 <vivianxhumetiku@gmail.com>
2022-07-21 03:03:31 -0700

    task 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -7,6 +7,16 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	unsigned int i = 0;
+	dlistint_t *tmp = head;
+
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	if (i < index)
+		return (NULL);
 	if (head == NULL)
 		return (NULL);
 	if (index == 0)
