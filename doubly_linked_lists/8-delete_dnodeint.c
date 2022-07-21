cf8cd65a471b41f8beeef4422269ecbc1a2ba3de
viviani22 <vivianxhumetiku@gmail.com>
2022-07-21 08:52:07 -0700

    task 8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - called from main
+ * @head: ptr to starting node
+ * @index: node to be deleted
+ * Return: 1 if successful, 0 otherwise
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = *head, *tmp2;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		if (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+			tmp->prev = NULL;
+		}
+		free(*head);
+		*head = tmp;
+		return(1);
+	}
+	while (index != 0 && tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		index--;
+	}
+	tmp2 = tmp->prev;
+	if (tmp->next == NULL)
+	{
+		tmp2->next = NULL;
+		tmp->prev = NULL;
+		free(tmp);
+	}
+	else
+	{
+		tmp2->next = tmp->next;
+		tmp2 = tmp2->next;
+		tmp2->prev = tmp->prev;
+		free(tmp);
+	}
+	return (1);
+}
