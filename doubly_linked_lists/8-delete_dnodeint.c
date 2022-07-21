3eb91c6c133dc1b245d1408ee094798aba1ad110
viviani22 <vivianxhumetiku@gmail.com>
2022-07-21 08:53:04 -0700

    task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -20,7 +20,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		}
 		free(*head);
 		*head = tmp;
-		return(1);
+		return (1);
 	}
 	while (index != 0 && tmp->next != NULL)
 	{
