04babcf3d55bde8e90b085c08f46bea48f5ea727
viviani22 <vivianxhumetiku@gmail.com>
2022-07-21 03:49:53 -0700

    task 7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - called from main
+ * @h: node
+ * @idx: int
+ * @n: int
+ * Return: node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *tmp = *h;
+
+	if (*h == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		new->prev = NULL;
+		new->next = tmp;
+		tmp->prev = new;
+		*h = new;
+		return (*h);
+	}
+	while (idx != 1 && tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		idx--;
+	}
+	if (idx == 1 && tmp->next == NULL)
+		return (NULL);
+	new->prev = tmp;
+	if (tmp->next->next != NULL)
+		new->next = tmp->next;
+	else
+		new->next = NULL;
+	tmp->next = new;
+	return (new);
+}
